98359b1a9ffd1bfa73af807f4aecb5f122c71f7a
Philipp Mandler <info@philipp-mandler.com>
2021-12-02 19:43:01 +0100

    Move common applications

--- hosts/common.nix
+++ hosts/common.nix
@@ -12,4 +12,28 @@
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
   system.stateVersion = 4;
+
+  environment.systemPackages = with pkgs; [
+    zsh
+    zsh-autosuggestions
+    zsh-syntax-highlighting
+
+    vim
+    git
+    htop
+    tmux
+    fd
+    neovim
+    ripgrep
+    tldr
+    tree
+    cloc
+
+    nodejs-slim-14_x
+    nodePackages.npm
+
+    python3
+    jq
+    yq
+  ];
 }
--- hosts/meins/configuration.nix
+++ hosts/meins/configuration.nix
@@ -1,38 +1,19 @@
 { config, pkgs, lib, ... }:
 {
-
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
-    vim
     postgresql_13
     redis
-    git
-    htop
-    tmux
-    zsh
-    neovim
-    ripgrep
-    tldr
     mas
     kubectl
     kubernetes-helm
-    cloc
     ktlint
     minicom
-    nodejs-slim-14_x
-    nodePackages.npm
     pandoc
-    python3
     terraform
-    tree
-    zsh-syntax-highlighting
-    zsh-autosuggestions
     nodePackages.serve
     rustup
-    fd
-    jq
-    yq
     gh
     argocd
     vscode
