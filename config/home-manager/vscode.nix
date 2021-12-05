d3cb51ef8fc4de567b31d917e9b35c12f6be6431
Philipp Mandler <info@philipp-mandler.com>
2021-12-05 19:53:46 +0100

    vscode: Use prettier as formatter

--- config/home-manager/vscode.nix
+++ config/home-manager/vscode.nix
@@ -3,6 +3,26 @@
     enable = true;
     userSettings = {
       "workbench.colorTheme" = "One Dark Pro";
+
+      # Language specific settings
+      "[javascript]" = {
+        "editor.defaultFormatter" = "esbenp.prettier-vscode";
+      };
+      "[javascriptreact]" = {
+        "editor.defaultFormatter" = "esbenp.prettier-vscode";
+      };
+      "[typescript]" = {
+        "editor.defaultFormatter" = "esbenp.prettier-vscode";
+      };
+      "[typescriptreact]" = {
+        "editor.defaultFormatter" = "esbenp.prettier-vscode";
+      };
+      "[json]" = {
+        "editor.defaultFormatter" = "esbenp.prettier-vscode";
+      };
+      "[graphql]" = {
+        "editor.defaultFormatter" = "esbenp.prettier-vscode";
+      };
     };
   };
 }
