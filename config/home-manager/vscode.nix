d6025b229f6e65525a5580f4c7ce1e11bccc4fdb
Philipp Mandler <info@philipp-mandler.com>
2021-12-08 09:48:58 +0100

    vscode: Add settings

--- config/home-manager/vscode.nix
+++ config/home-manager/vscode.nix
@@ -2,7 +2,9 @@
   programs.vscode = {
     enable = true;
     userSettings = {
+      # Visuals
       "workbench.colorTheme" = "One Dark Pro";
+      "workbench.iconTheme" = "seti";
 
       # Language specific settings
       "[javascript]" = {
@@ -23,6 +25,102 @@
       "[graphql]" = {
         "editor.defaultFormatter" = "esbenp.prettier-vscode";
       };
+      "[markdown]" = {
+        "editor.defaultFormatter": "esbenp.prettier-vscode";
+      };
+
+      # Generally ignored files (e.g. are not displayed in the explorer)
+      "files.exclude" = {
+        "**/.classpath" = true;
+        "**/.factorypath" = true;
+        "**/.idea" = true;
+        "**/.project" = true;
+        "**/.settings" = true;
+      };
+
+      # Files excluded from search
+      "search.exclude" = {
+        "**/.cache" = true;
+        "**/.next" = true;
+        "**/dist" = true;
+        "**/node_modules" = true;
+        "**/target" = true;
+      };
+
+      # Misc
+      "terminal.integrated.scrollback" = 20000;
+      "markdown-preview-enhanced.breakOnSingleNewLine" = false;
     };
   };
+
+  keybindings = [
+    {
+        "key" = "cmd+[IntlBackslash]";
+        "command" = "workbench.action.terminal.toggleTerminal";
+    }
+    {
+        "key" = "ctrl+[Period]";
+        "command" = "editor.action.marker.next";
+        "when" = "editorFocus";
+    }
+    {
+        "key" = "cmd+k n";
+        "command" = "-editor.action.marker.next";
+        "when" = "editorFocus";
+    }
+    {
+        "key" = "ctrl+shift+[Period]";
+        "command" = "editor.action.marker.prev";
+        "when" = "editorFocus";
+    }
+    {
+        "key" = "cmd+k p";
+        "command" = "-editor.action.marker.prev";
+        "when" = "editorFocus";
+    }
+    {
+        "key" = "cmd+0";
+        "command" = "-workbench.action.focusSideBar";
+    }
+    {
+        "key" = "cmd+0";
+        "command" = "workbench.action.focusSideBar";
+        "when" = "editorFocus";
+    }
+    {
+        "key" = "cmd+0";
+        "command" = "workbench.action.focusActiveEditorGroup";
+        "when" = "!editorFocus";
+    }
+    {
+        "key" = "ctrl+alt+down";
+        "command" = "markdown.extension.onMoveLineDown";
+        "when" = "editorTextFocus && !editorReadonly && !suggestWidgetVisible && editorLangId == 'markdown'";
+    }
+    {
+        "key" = "alt+down";
+        "command" = "-markdown.extension.onMoveLineDown";
+        "when" = "editorTextFocus && !editorReadonly && !suggestWidgetVisible && editorLangId == 'markdown'";
+    }
+    {
+        "key" = "ctrl+alt+up";
+        "command" = "markdown.extension.onMoveLineUp";
+        "when" = "editorTextFocus && !editorReadonly && !suggestWidgetVisible && editorLangId == 'markdown'";
+    }
+    {
+        "key" = "alt+up";
+        "command" = "-markdown.extension.onMoveLineUp";
+        "when" = "editorTextFocus && !editorReadonly && !suggestWidgetVisible && editorLangId == 'markdown'";
+    }
+    {
+        "command" = "footsteps.skipBack";
+        "key" = "ctrl+alt+left";
+        "mac" = "ctrl+alt+left";
+    }
+    {
+        "command" = "footsteps.skipForwards";
+        "key" = "ctrl+alt+right";
+        "mac" = "ctrl+alt+right";
+    }
+  ];
 }
