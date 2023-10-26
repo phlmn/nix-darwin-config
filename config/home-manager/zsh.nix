9e592bdcdcaea28dbeffc7821d5f1b6dfd55f21b
Philipp Mandler <info@philipp-mandler.com>
2023-10-26 16:13:14 +0200

    Update zsh config

--- config/home-manager/zsh.nix
+++ config/home-manager/zsh.nix
@@ -2,7 +2,7 @@
 {
   programs.zsh = {
     enable = true;
-    enableSyntaxHighlighting = true;
+    syntaxHighlighting.enable = true;
     enableCompletion = true;
     enableAutosuggestions = true;
 
