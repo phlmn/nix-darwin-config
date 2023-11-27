a337562dc233304be6893704fa209fea3ebd18f6
Philipp Mandler <info@philipp-mandler.com>
2023-11-27 21:57:52 +0100

    Enable flakes

--- profiles/base.nix
+++ profiles/base.nix
@@ -5,6 +5,7 @@
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
   nix.package = pkgs.nix;
+  nix.settings.experimental-features = [ "nix-command" "flakes"];
 
   # Create /etc/bashrc that loads the nix-darwin environment.
   programs.zsh.enable = true;
